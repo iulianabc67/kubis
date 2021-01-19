38dfddb340f1b2715d8dc4339d9ba1feb2246119
iulian <iulian.abc@gmail.com>
2021-01-19 23:24:54 +0200

    change tailwind css link

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $item->name }}</title>
     <!-- Tailwind CSS -->
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css'>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    {{-- <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/tailwindcss@2.0.1/dist/tailwind.min.css'> --}}
 </head>
 
 <body class="bg-gray-500">
